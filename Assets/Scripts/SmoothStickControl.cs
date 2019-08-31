73bb5d0da64faec3c107eb4d231f444c3240b831
Barnaby <barnaby.rw@gmail.com>
2019-08-31 11:20:38 +1000

    bop

--- Assets/Scripts/SmoothStickControl.cs
+++ Assets/Scripts/SmoothStickControl.cs
@@ -1,18 +1,46 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+using System;
+// The GameObject is made to bounce using the space key.
+// Also the GameOject can be moved forward/backward and left/right.
+// Add a Quad to the scene so this GameObject can collider with a floor.
 
 public class SmoothStickControl : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public float speed = 6.0f;
+
+    //public float gravity = 20.0f;
+
+    private Vector3 moveDirection = Vector3.zero;
+    private CharacterController controller;
+
+
+    public float _rotationSpeed = 180;
+
+ 
     void Start()
     {
-        
+        controller = GetComponent<CharacterController>();
+
+        // let the gameObject fall down
+        //gameObject.transform.position = new Vector3(0, 5, 0);
     }
 
-    // Update is called once per frame
     void Update()
     {
+      
+            // We are grounded, so recalculate
+            // move direction directly from axes
+
+            moveDirection = new Vector3(Input.GetAxis("HorizontalB"), Input.GetAxis("VerticalB"), Input.GetAxis("Vertical"));
+            moveDirection = transform.TransformDirection(moveDirection);
+            moveDirection = moveDirection * speed;
         
+
+        // Apply gravity
+       // moveDirection.y = moveDirection.y - (gravity * Time.deltaTime);
+
+        // Move the controller
+        controller.Move(moveDirection * Time.deltaTime);
     }
 }
